45f07129a5dc1d3ef96f7c2cc01ebc2831ad8171
Snoow17 <j.rinzen@live.se>
2025-04-13 10:14:57 +0200

    Added Search Function

---
+++ BrodcastSocialMedia/BrodcastSocialMedia/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using BrodcastSocialMedia.Data;
+using BrodcastSocialMedia.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrodcastSocialMedia.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public UsersController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index(UsersIndexViewModel viewModel)
+        {
+            if(viewModel.Search != null)
+            {
+                var users = await _dbContext.Users.Where(u => u.Name.Contains(viewModel.Search))
+                                .ToListAsync();
+                viewModel.Result = users;
+            }
+            
+
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ BrodcastSocialMedia/BrodcastSocialMedia/ViewModels/UsersIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BrodcastSocialMedia.Models;
+
+namespace BrodcastSocialMedia.ViewModels
+{
+    public class UsersIndexViewModel
+    {
+        public string Search { get; set; }
+        public List<ApplicationUser> Result { get; set; } = new List<ApplicationUser>();
+    }
+}
