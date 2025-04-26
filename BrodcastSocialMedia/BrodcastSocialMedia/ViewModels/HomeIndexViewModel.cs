a8b80f322a3254314d8d4c36957a19bae0d08745
Snoow17 <j.rinzen@live.se>
2025-04-26 09:26:39 +0200

    Show Broadcasts on StartPage

---
+++ BrodcastSocialMedia/BrodcastSocialMedia/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BrodcastSocialMedia.Data.Migrations;
+using BrodcastSocialMedia.Models;
+
+namespace BrodcastSocialMedia.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public List<Broadcast> Broadcasts { get; set; }
+    }
+}
